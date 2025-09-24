ec12c66ef0ff077d8f61cbec2b4201c4c40cd0e5
Ryannvs <ryannvs89@gmail.com>
2025-09-24 11:27:25 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+/******************************************************************************************************
+ * Objetivo: EndPoints referente a API de contatos do whatsapp
+ * Data: 24/09/25
+ * Autor: Marcos Ryan
+ * Versão: 1.0
+ * 
+ * Observações: Instalação do Express, Cors, Body-Parser
+ * npm install express --save
+ * npm install cors express --save
+ * npm install body-parser --save
+ ******************************************************************************************************/
---
+++ module/funcoes.js
@@ -0,0 +1,24 @@
+/******************************************************************************************************
+* Objetivo: Arquivo de funções para gerenciar a API de contatos do whatsapp
+* Data: 24/09/25
+* Autor: Marcos Ryan
+* Versão: 1.0
+******************************************************************************************************/
+
+const MESSAGE_ERRO = {
+    status: false,
+    status_code: 500,
+    development: 'Marcos Ryan'
+}
+const dados = require('./contatos.js')
+
+const getAllContatos = function(){}
+
+const getDadosConta = function(){}
+
+const getDadosContato = function(){}
+
+const getMensagensContato = function(){}
+
+const getConversaContato = function(){}
+
